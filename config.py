02b67a5946f1c4a3ffc0787598d0b784f35082a1
ruslankyzyljol-ai <ruslan.kyzyljol@gmail.com>
2026-06-20 18:06:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,22 +5,5 @@ ADMIN_ID = 6954551561
 CHANNEL_USERNAME = "@profixkg"
 CHANNEL_URL = "https://t.me/profixkg"
 
-# Папкалар
 IMAGES_DIR = "images"
 MINES_DIR = os.path.join(IMAGES_DIR, "mines")
-
-# Баштапкы маалыматтар (JSON аркылуу сакталат)
-DEFAULT_DATA = {
-    "links": {
-        "1win": "https://one-vv0073.com/?p=hors",
-        "mostbet": "https://r7ycnmimb.com/K9mU",
-        "melbet": "",
-        "1xbet": ""
-    },
-    "promocodes": {
-        "1win": "PROFIN",
-        "mostbet": "PROFIX",
-        "melbet": "",
-        "1xbet": ""
-    }
-}
