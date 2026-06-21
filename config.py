8d70b4241143de8ac2e7218704488ec48748255c
ruslankyzyljol-ai <ruslan.kyzyljol@gmail.com>
2026-06-22 00:27:53 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,15 @@
-import os
-
 BOT_TOKEN = "8941229692:AAHUlPDh394sv0Y8OtqPQluigVQGWY6wehY"
 ADMIN_ID = 6954551561
 CHANNEL_USERNAME = "@profixkg"
 CHANNEL_URL = "https://t.me/profixkg"
+MINES_DIR = "images"
 
-IMAGES_DIR = "images"
-MINES_DIR = os.path.join(IMAGES_DIR, "mines")
+LINKS = {
+    "1win": "https://one-vv0073.com/?p=hors",
+    "mostbet": "https://r7ycnmimb.com/K9mU"
+}
+
+PROMOS = {
+    "1win": "PROFIN",
+    "mostbet": "PROFIX"
+}
